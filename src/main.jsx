08a70d6ab21cf8122611dc2de1bdf96c0ffd5743
wishwadeepa <sharadawishwadeepa904@gmail.com>
2025-12-01 00:07:36 +0530

    Initialize main.jsx for Flood Simulator app

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FloodSimulatorApp from './flood.jsx'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FloodSimulatorApp />
+  </React.StrictMode>,
+)
